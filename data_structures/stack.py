a610c19bea7171ca272f58da035a6dcc27b83ddf
tab1s4 <tabishghouri3@gmail.com>
2024-07-22 19:07:53 -0400

    finished stack class and added comments

--- data_structures/stack.py
+++ data_structures/stack.py
@@ -1,3 +1,24 @@
 class Stack:
     def __init__(self):
-        self.items = []
+        self.items = [] # initalizes an empty list named 'items' to store elements in the stack 
+
+    def is_empty(self):
+        return len(self.items) == 0 # returns true if the stack is empty
+    
+    def push(self, item):
+        self.items.append(item) # appends 'item' to the end of the list named 'items'
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        return None
+        # if stack is not empty then remove item at the top of the stack, otherwise do nothing
+    
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        return None
+        # if stack is not empty return the last from the list (top of the stack) without removing it
+    
+    def size(self):
+        return len(self.items) # return the size of the stack
